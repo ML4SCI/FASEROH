726e2aa3ddeb565fd0e765b522e0fb15f458ba97
asmayer <asmayer1216@gmail.com>
2022-10-21 14:21:08 -0600

    primarily added poison noise and a check to see if the histogram is positive
    also edited commadline args of train.py

--- src/datasetgen.py
+++ src/datasetgen.py
@@ -3,7 +3,7 @@ import expression
 parser = argparse.ArgumentParser(description='Genereates a dataset of expressions')
 parser.add_argument('--num_ops', type=int, default=5)
 parser.add_argument('--items', type=int, default=10)
-parser.add_argument('--bins', type=int, default=10)
+parser.add_argument('--bins', type=int, default=5)
 parser.add_argument('--path_funcs', type=str, default='funcs.csv')
 parser.add_argument('--path_hist', type=str, default='hist.csv')
 args = parser.parse_args()
